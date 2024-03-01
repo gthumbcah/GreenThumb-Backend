c2c9cd95bbadb89cca668e890c17198aa05eb539
hman09 <14950@coderacademy.edu.au>
2024-03-01 20:42:28 +1100

    job CRUD finished

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -21,130 +21,136 @@ describe('App Test', () => {
         
     })
 
-    // // test on the entry route
-    // test('GET /', async () => {
-    //     const user = await request(app).get('/')
-    //     expect(user.status).toBe(200)
-    //     expect(user.header['content-type']).toContain('json')
-    //     expect(user.body.info).toBe('Hello!')
-    // })
+    // test on the entry route
+    test('GET /', async () => {
+        const user = await request(app).get('/')
+        expect(user.status).toBe(200)
+        expect(user.header['content-type']).toContain('json')
+        expect(user.body.info).toBe('Hello!')
+    })
 
 
-    // // Login API ---------------------------------------------------------
-    // describe('Login POST test', () => {
+    // Login API ---------------------------------------------------------
+    describe('Login POST test', () => {
 
-    //     let user, id
+        let user, id
 
-    //     beforeAll(async () => {
-    //         user = await request(app).post('/users').set('Authorization', `Bearer ${token}`).send({
-    //             name: 'tester',
-    //             email: 'tester@email.com',
-    //             password: 'tester'
-    //         })
-    //         id = user.body.id
-    //     })
+        beforeAll(async () => {
+            user = await request(app).post('/users').set('Authorization', `Bearer ${token}`).send({
+                name: 'tester',
+                email: 'tester@email.com',
+                password: 'tester'
+            })
+            id = user.body.id
+        })
 
-    //     // User created successfully
-    //     test('Returns JSON content', async () => {
-    //         expect(user.status).toBe(201)
-    //         expect(user.header['content-type']).toContain('json')
-    //         expect(user.body.name).toBe('tester')
-    //     })
+        // User created successfully
+        test('Returns JSON content', async () => {
+            expect(user.status).toBe(201)
+            expect(user.header['content-type']).toContain('json')
+            expect(user.body.name).toBe('tester')
+        })
 
-    //     // invalid email
-    //     test('/login with invalid email', async () => {
-    //         const login = await request(app).post('/login').send({
-    //             email: 'failtest',
-    //             password: 'tester'
-    //         })
-    //         expect(login.status).toBe(404)
-    //         expect(login.header['content-type']).toContain('json')
-    //         expect(login.body.message).toBe('User not found')
-    //     })
+        // invalid email
+        test('/login with invalid email', async () => {
+            const login = await request(app).post('/login').send({
+                email: 'failtest',
+                password: 'tester'
+            })
+            expect(login.status).toBe(404)
+            expect(login.header['content-type']).toContain('json')
+            expect(login.body.message).toBe('User not found')
+        })
 
-    //     // invalid Password
-    //     test('/login with invalid Password', async () => {
-    //         const login = await request(app).post('/login').send({
-    //             email: 'tester@email.com',
-    //             password: 'failtest'
-    //         })
-    //         expect(login.status).toBe(403)
-    //         expect(login.header['content-type']).toContain('json')
-    //         expect(login.body.message).toBe('Invalid password')
-    //     })
+        // invalid Password
+        test('/login with invalid Password', async () => {
+            const login = await request(app).post('/login').send({
+                email: 'tester@email.com',
+                password: 'failtest'
+            })
+            expect(login.status).toBe(403)
+            expect(login.header['content-type']).toContain('json')
+            expect(login.body.message).toBe('Invalid password')
+        })
 
-    //     // Successful login
-    //     test('/login Successful', async () => {
-    //         const login = await request(app).post('/login').send({
-    //                 email: 'tester@email.com',
-    //                 password: 'tester'
-    //             })
-    //         expect(login.status).toBe(200)
-    //         expect(login.body).toHaveProperty('token')
-    //     })
+        // Successful login
+        test('/login Successful', async () => {
+            const login = await request(app).post('/login').send({
+                    email: 'tester@email.com',
+                    password: 'tester'
+                })
+            expect(login.status).toBe(200)
+            expect(login.body).toHaveProperty('token')
+        })
         
-    //     afterAll(async () => {
-    //         // Cleanup
-    //         const cleanUp = await request(app).delete(`/users/${id}`).set('Authorization', `Bearer ${token}`)
-    //     })
-    // })
+        afterAll(async () => {
+            // Cleanup
+            const cleanUp = await request(app).delete(`/users/${id}`).set('Authorization', `Bearer ${token}`)
+        })
+    })
 
-    // // User API ---------------------------------------------------------
-    // describe('User CRUD', () => {
+    // User API ---------------------------------------------------------
+    describe('User CRUD', () => {
 
-    //     let user, id
+        let user, id
 
-    //     beforeEach(async () => {
-    //         user = await request(app).post('/users').set('Authorization', `Bearer ${token}`).send({
-    //             name: 'tester',
-    //             email: 'tester@email.com',
-    //             password: 'tester',
-    //         })
-    //         id = user.body.id
-    //     })
+        beforeEach(async () => {
+            user = await request(app).post('/users').set('Authorization', `Bearer ${token}`).send({
+                name: 'tester',
+                email: 'tester@email.com',
+                password: 'tester',
+            })
+            id = user.body.id
+        })
 
 
-    //     // Confirms the POST is working
-    //     test('Returns JSON content | confirms user post', () => {
-    //         expect(user.status).toBe(201)
-    //         expect(user.header['content-type']).toContain('json')
-    //         expect(user.body.name).toBe('tester')
-    //         expect(user.body.id).toBe(id)
+        // Confirms the POST is working
+        test('Returns JSON content | confirms user post', () => {
+            expect(user.status).toBe(201)
+            expect(user.header['content-type']).toContain('json')
+            expect(user.body.name).toBe('tester')
+            expect(user.body.id).toBe(id)
             
-    //     })
+        })
 
-    //     // Confirms the GET by id is working
-    //     test('READ /users/:id', async () => {
-    //         const u = await request(app).get(`/users/${id}`).set('Authorization', `Bearer ${token}`)
-    //         expect(u.status).toBe(200)
-    //         expect(u.header['content-type']).toContain('json')
-    //         expect(u.body.name).toBe('tester')
-    //     })
+        // Confirms the GET by id is working
+        test('READ /users/:id', async () => {
+            const u = await request(app).get(`/users/${id}`).set('Authorization', `Bearer ${token}`)
+            expect(u.status).toBe(200)
+            expect(u.header['content-type']).toContain('json')
+            expect(u.body.name).toBe('tester')
+        })
 
-    //     // Confirms the PUT by id is working
-    //     test('UPDATE /users/:id', async () => {
-    //         const u = await request(app).put(`/users/${id}`).set('Authorization', `Bearer ${token}`).send({
-    //             name: 'tester2',
-    //             email: 'tester2@email.com',
-    //             password: 'tester2',
-    //         })
-    //         expect(u.status).toBe(200)
-    //         expect(u.header['content-type']).toContain('json')
-    //         expect(u.body.name).toBe('tester2')
-    //         expect(u.body.email).toBe('tester2@email.com')
-    //     })
+        // Confirms the PUT by id is working
+        test('UPDATE /users/:id', async () => {
+            const u = await request(app).put(`/users/${id}`).set('Authorization', `Bearer ${token}`).send({
+                name: 'tester2',
+                email: 'tester2@email.com',
+                password: 'tester2',
+            })
+            expect(u.status).toBe(200)
+            expect(u.header['content-type']).toContain('json')
+            expect(u.body.name).toBe('tester2')
+            expect(u.body.email).toBe('tester2@email.com')
+        })
 
-    //     afterEach(async () => {
-    //         // Cleanup
-    //         const cleanUp = await request(app).delete(`/users/${id}`).set('Authorization', `Bearer ${token}`)
-    //     })
+        afterEach(async () => {
+            // Cleanup
+            const cleanUp = await request(app).delete(`/users/${id}`).set('Authorization', `Bearer ${token}`)
+        })
 
-    // })
+    })
 
     // Job API ----------------------------------------------------------------
     describe('Job CRUD', () => {
-
         let user, userId, job, jobId, currentJob, currentJobUser
+
+        const jobDetails = {
+            customerDetails: ['Testy Test', '0411111111', 'https://maps.app.goo.gl/TEST1111111111111'],
+            toolsNeeded: ['TestTool1', 'TestTool2'],
+            tasks: ['TestTask1', 'TestTask2'],
+            dates: ["2024-02-24", "2024-02-25", "2024-02-26"]
+        }
 
         beforeEach(async () => {
             user = await request(app).post('/users').set('Authorization', `Bearer ${token}`).send({
@@ -154,11 +160,8 @@ describe('App Test', () => {
             })
             userId = user.body.id
             job = await request(app).post('/jobs').set('Authorization', `Bearer ${token}`).send({
-                customerDetails: ['Testy Test', '0411111111', 'https://maps.app.goo.gl/QPJXtBMv3iZsBCYd7'],
-                toolsNeeded: ['TestTool1', 'TestTool2'],
-                users: [ userId],
-                tasks: ['TestTask1', 'TestTask2'],
-                dates: ["2024-02-24","2024-02-25","2024-02-26"]
+                ...jobDetails,
+                users: userId
             })
             jobId = job.body._id
             currentJob = await request(app).get(`/jobs/${jobId}`).set('Authorization', `Bearer ${token}`)
@@ -171,7 +174,7 @@ describe('App Test', () => {
             expect(jobResponse.header['content-type']).toContain('json')
             expect(jobResponse.body.customerDetails).toEqual(expect.arrayContaining(expectedDetails))
             expect(jobResponse.body.toolsNeeded).toEqual(expect.arrayContaining(expectedTools))
-            expect(jobResponse.body.users).toEqual(expect.arrayContaining(expectedUserId))
+            expect(jobResponse.body.users).toEqual(expect.arrayContaining([expect.objectContaining({_id : expectedUserId})]))
             expect(jobResponse.body.tasks).toEqual(expect.arrayContaining(expectedTasks))
             expect(jobResponse.body._id).toBe(expectedId)
         }
@@ -184,33 +187,30 @@ describe('App Test', () => {
             expect(user.body.id).toBe(userId)           
         })
 
-        // error recieved on request. close answer but object
-            // expect(received).toEqual(expected) // deep equality
-
-    // Expected: ArrayContaining ["65e1605e8c2447bff31cc85e"]
-    // Received: [{"__v": 0, "_id": "65e1605e8c2447bff31cc85e", "admin": false, "email": "tester@email.com", "name": "tester", "password": "$2b$10$Z151mFN5jeqKhgP1Wr.ztOxzuT/P/nxLN5XJIsEXWEFzrDkfV4Edi"}]
         // Confirms the job POST is working
-        // test('confirms job post returns JSON content', async () => {            
-        //     jobTest(job, ['Testy Test', '0411111111', 'https://maps.app.goo.gl/QPJXtBMv3iZsBCYd7'], ['TestTool1', 'TestTool2'], [userId], ['TestTask1', 'TestTask2'], jobId)
-        // })
+        test('confirms job post returns JSON content', async () => {            
+            jobTest(job, jobDetails.customerDetails, jobDetails.toolsNeeded, userId, jobDetails.tasks, jobId)
+        })
 
-        // // Confirms the job GET by id is working
-        // test('READ /jobs/:id', async () => {
-        //     jobTest(job, ['Testy Test', '0411111111', 'https://maps.app.goo.gl/QPJXtBMv3iZsBCYd7'], ['TestTool1', 'TestTool2'], [userId], ['TestTask1', 'TestTask2'], jobId)
-        // })
+        // Confirms the job GET by id is working
+        test('READ /jobs/:id', async () => {
+            jobTest(job, jobDetails.customerDetails, jobDetails.toolsNeeded, userId, jobDetails.tasks, jobId)
+        })            
 
-            
-
-        // // Confirms the jobs PUT by id is working
-        // test('UPDATE /jobs/:id', async () => {
-        //     const j = await request(app).put(`/jobs/${jobId}`).send({
-        //         customerDetails: ['changeName', 'changeMob', 'changeAddress'],
-        //         toolsNeeded: ['changeTool'],
-        //         users: [ userId ],
-        //         tasks: ['changeTask']
-        //     })
-        //     jobTest(j, ['Testy Test', '0411111111', 'https://maps.app.goo.gl/QPJXtBMv3iZsBCYd7'], ['TestTool1', 'TestTool2'], [userId], ['TestTask1', 'TestTask2'], jobId)
-        // })
+        // Confirms the jobs PUT by id is working
+        test('UPDATE /jobs/:id', async () => {
+            const customerDetails = ['Tosty Toot', '0422222222', 'https://maps.app.goo.gl/TEST2222222222222']
+            const toolsNeeded = ['ChangedTool1', 'ChangedTool2']
+            const tasks = ['ChangedTask1', 'ChangedTask2']
+        
+            const j = await request(app).put(`/jobs/${jobId}`).send({
+                customerDetails: customerDetails,
+                toolsNeeded: toolsNeeded,
+                users: userId,
+                tasks: tasks
+            })
+            jobTest(j, customerDetails, toolsNeeded, userId, tasks, jobId)
+        })
 
         afterEach(async () => {
             // Cleanup
@@ -218,6 +218,5 @@ describe('App Test', () => {
             const cleanUpUser = await request(app).delete(`/users/${userId}`).set('Authorization', `Bearer ${token}`)
         })
     })
-
 })
 
